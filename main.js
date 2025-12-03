e6775d22291a9acac7c8993a5b426e78650c9d39
Lucas Monte <lucasmonteverdeee@gmail.com>
2025-12-03 00:03:04 -0300

    resizable and  autoHideMenuBar

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, nativeTheme } = require('electron')
 
 const createWindow = () => {
+  nativeTheme.themeSource = 'system' // Can be 'dark' or 'light'
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: './src/public/img/folha-de-carvalho.png',
+    resizable: false,
+    autoHideMenuBar: true,
+    //titleBarStyle: 'hidden'
   })
 
   win.loadFile('./src/views/index.html')
